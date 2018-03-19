a48521913512c143780b3558dbb55fa0b7ff556a
yxue5 <yxue5@buffalo.edu>
2018-03-19 19:58:18 -0400

    Some more stuff for Issue 7
    Apparently I need these too in order to commit

--- ProjectEroica/Source/ProjectEroica/AnimInstanceKisa.cpp
+++ ProjectEroica/Source/ProjectEroica/AnimInstanceKisa.cpp
@@ -1,6 +1,13 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "AnimInstanceKisa.h"
+#include "Kismet/GameplayStatics.h"
 
-
-
+void UAnimInstanceKisa::HandleState(FString newState) {
+	if (newState != State) {
+		if (State == "Dash") {
+			UGameplayStatics::PlaySound2D(this, DashSound);
+		}
+	}
+	State = newState;
+}
