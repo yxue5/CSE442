d60ded0913e67d3fd949d1f116163668becc59eb
kolung <36254190+kolung@users.noreply.github.com>
2018-03-27 16:24:58 -0400

    Update Weapon.cpp
    code comment done:)

--- ProjectEroica/Source/ProjectEroica/Weapon.cpp
+++ ProjectEroica/Source/ProjectEroica/Weapon.cpp
@@ -8,18 +8,26 @@
 //#include "Runtime/Engine/Classes/Components/StaticMeshComponent.h"
 
 // Sets default values
+
 AWeapon::AWeapon()
 {
+	// Get weapon mesh combo from UStaticMeshComponent 
 	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("WepMesh"));
+	// Set collision enabled for mesh
 	MeshComp->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	// Set collision response for mesh
 	MeshComp->SetCollisionResponseToAllChannels(ECR_Ignore);
 	RootComponent = MeshComp;
 	
+	// Get Capsule component from UCapsuleComponent 
 	CapsuleComponent = CreateDefaultSubobject<UCapsuleComponent>(TEXT("CapsuleComponent"));
+	// Set collision enabled for capsule component
 	CapsuleComponent->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	// Set collision response for capsule component 
 	CapsuleComponent->SetCollisionResponseToAllChannels(ECR_Ignore);
-
+	// Get collsion weapon for capsule component
 	CapsuleComponent->OnComponentBeginOverlap.AddDynamic(this, &AWeapon::OnWeaponBeginOverlap);
+	// Set attackingment for capsule component 
 	CapsuleComponent->SetupAttachment(MeshComp);
 }
 
@@ -27,6 +35,7 @@ AWeapon::AWeapon()
 void AWeapon::OnWeaponBeginOverlap(UPrimitiveComponent * OverlappedComponent, AActor * OtherActor, 
 	UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
 {
+	// Create warnings for weapon overlapped
 	UE_LOG(LogTemp, Warning, TEXT("Weapon overlapped actor!"));
 	ATP_SideScrollerCharacter* validChar = Cast<ATP_SideScrollerCharacter>(OtherActor);
 	if (validChar && validChar!= wepOwner) {
